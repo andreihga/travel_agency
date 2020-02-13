b486a599595a0f5dcaa4cb27e41d16088a5848f2
hogeaaa <andreihga@gmail.com>
2020-02-13 17:25:46 +0200

    - insert airports via excel finished - next, insert flights

--- src/main/java/org/com/sda/logic/readers/AirportReader.java
+++ src/main/java/org/com/sda/logic/readers/AirportReader.java
@@ -1,4 +1,55 @@
 package org.com.sda.logic.readers;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.com.sda.dto.AirportDTO;
+import org.com.sda.dto.CityDTO;
+import org.com.sda.dto.CountryDTO;
+import org.com.sda.logic.consts.Consts;
+import org.com.sda.logic.service.AirportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+@Service
 public class AirportReader {
+    @Autowired
+    private AirportService airportService;
+
+    public void readAirportsFromFile() {
+        try {
+            FileInputStream file = new FileInputStream(new File(Consts.EXCEL_FILE_PATH));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(2);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    AirportDTO airportDTO = new AirportDTO();
+                    airportDTO.setAirportNameDTO(cell.getStringCellValue());
+                    cell = cellIterator.next();
+                    CityDTO cityDTO = new CityDTO();
+                    cityDTO.setCityName(cell.getStringCellValue());
+                    cell = cellIterator.next();
+                    CountryDTO countryDTO = new CountryDTO();
+                    countryDTO.setCountryName(cell.getStringCellValue());
+                    cityDTO.setCountryDTO(countryDTO);
+                    airportDTO.setCityDTO(cityDTO);
+                    airportService.addAirport(airportDTO);
+                }
+            }
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/com/sda/rest/AirportController.java
+++ src/main/java/org/com/sda/rest/AirportController.java
@@ -1,6 +1,7 @@
 package org.com.sda.rest;
 
 import org.com.sda.dto.AirportDTO;
+import org.com.sda.logic.readers.AirportReader;
 import org.com.sda.logic.service.AirportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +14,28 @@ import java.util.List;
 public class AirportController {
     @Autowired
     private AirportService airportService;
+    @Autowired
+    private AirportReader airportReader;
 
     @PutMapping(value = "/addAirport", consumes = "application/json")
-    private ResponseEntity addAirport(@RequestBody AirportDTO airportDTO){
+    private ResponseEntity addAirport(@RequestBody AirportDTO airportDTO) {
         airportService.addAirport(airportDTO);
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping(value = "/addAirportsViaExcel")
+    private ResponseEntity addAirportViaExcel() {
+        airportReader.readAirportsFromFile();
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping("/getAirports")
-    private ResponseEntity getAirports(){
+    private ResponseEntity getAirports() {
+
         List<AirportDTO> airportDTOList = airportService.getAirports();
 
         return ResponseEntity.ok(airportDTOList);
     }
+
 
 }
