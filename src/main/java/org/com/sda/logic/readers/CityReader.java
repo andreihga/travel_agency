c75f254195798c68121071f1a86d423c0007f4e8
hogeaaa <andreihga@gmail.com>
2020-02-13 16:43:38 +0200

    - insert country via excel finished - next, insert city

---
+++ src/main/java/org/com/sda/logic/readers/CityReader.java
@@ -0,0 +1,5 @@
+package org.com.sda.logic.readers;
+
+public class CityReader {
+
+}
--- src/main/java/org/com/sda/logic/readers/CountryReader.java
+++ src/main/java/org/com/sda/logic/readers/CountryReader.java
@@ -1,5 +1,53 @@
 package org.com.sda.logic.readers;
 
-public class CountryReader {
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.com.sda.dto.ContinentDTO;
+import org.com.sda.dto.CountryDTO;
+import org.com.sda.logic.consts.Consts;
+import org.com.sda.logic.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class CountryReader {
+    @Autowired
+    private CountryService countryService;
+
+    public void readCountryFromFile() {
+        try {
+            FileInputStream file = new FileInputStream(new File(Consts.EXCEL_FILE_PATH));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()){
+                    Cell cell = cellIterator.next();
+                    CountryDTO countryDTO = new CountryDTO();
+                    countryDTO.setCountryName(cell.getStringCellValue());
+                    cell = cellIterator.next();
+                    ContinentDTO continentDTO = new ContinentDTO();
+                    continentDTO.setContinentName(cell.getStringCellValue());
+                    countryDTO.setContinentDTO(continentDTO);
+                    countryService.addCountries(countryDTO);
+                }
+            }
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
