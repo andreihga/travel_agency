b4ca146f7be9dd35e2efb683de479a74205836c8
hogeaaa <andreihga@gmail.com>
2020-02-13 14:30:57 +0200

    - started working on readFromExcel branch

--- src/main/java/org/com/sda/logic/service/ContinentService.java
+++ src/main/java/org/com/sda/logic/service/ContinentService.java
@@ -1,5 +1,8 @@
 package org.com.sda.logic.service;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellReference;
 import org.com.sda.dto.ContinentDTO;
 import org.com.sda.entity.Continent;
 import org.com.sda.repository.ContinentDAO;
@@ -25,6 +28,21 @@ public class ContinentService {
         //continentDAO.addContinent(continent);
 
         return continent;
+    }
+
+    public void readContentFromExcel(){
+        Workbook wb = new HSSFWorkbook();
+        DataFormatter formatter = new DataFormatter();
+        Sheet sheet = wb.getSheetAt(0);
+        for (Row row:sheet){
+            for (Cell cell : row){
+                CellReference cellReference = new CellReference(row.getRowNum(),cell.getColumnIndex());
+                System.out.println(cellReference.formatAsString());
+                System.out.println(" - ");
+                String text = formatter.formatCellValue(cell);
+                System.out.println(text);
+            }
+        }
     }
 
     public Continent getContinentByName(ContinentDTO continentDTO){
